d57adfb37d532eb901e4a7fa7ac981b4aa9d8e23
Mohamed <mouh4945@gmail.com>
2024-12-31 21:16:45 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,22 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text
+        style={{
+          fontSize: 24,
+          color: "#333",
+        }}
+      >
+        Welcome in typescript Dec 30 2024!
+      </Text>
+    </View>
+  );
+}
